670311780492f8e3dbf20aca6e3a102742839f39
vvcb <vvcb.n1@gmail.com>
2025-04-22 23:03:31 +0100

    Exceptions, table filtering query caching
    - move exceptions to separate module
    - add simple lru_cache to query functions
    - improve allowed table logic using SQLglot

---
+++ src/omcp/exceptions.py
@@ -0,0 +1,49 @@
+# Define more specific error classes for better error handling
+
+
+class QueryError(Exception):
+    """Base exception raised for errors in the query execution"""
+
+    pass
+
+
+class AmbiguousReferenceError(QueryError):
+    """Exception raised when a column reference is ambiguous"""
+
+    pass
+
+
+class ColumnNotFoundError(QueryError):
+    """Exception raised when a column referenced in the query doesn't exist"""
+
+    pass
+
+
+class EmptyQueryError(QueryError):
+    """Exception raised when the query is empty"""
+
+    pass
+
+
+class NotSelectQueryError(QueryError):
+    """Exception raised when a non-SELECT query is attempted"""
+
+    pass
+
+
+class SqlSyntaxError(QueryError):
+    """Exception raised for SQL syntax errors"""
+
+    pass
+
+
+class TableNotFoundError(QueryError):
+    """Exception raised when a table referenced in the query doesn't exist"""
+
+    pass
+
+
+class UnauthorizedTableError(QueryError):
+    """Exception raised when query attempts to access unauthorized tables"""
+
+    pass
