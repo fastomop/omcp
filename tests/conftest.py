04000ab2f1d30df0699063df5aaabd43adf57207
vvcb <vvcb.n1@gmail.com>
2025-04-26 13:38:20 +0100

    Add pytests

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+"""
+Pytest configuration file.
+Contains shared fixtures and configuration for the test suite.
+"""
+
+import sys
+import os
+
+# Add the src directory to the path so we can import modules correctly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
